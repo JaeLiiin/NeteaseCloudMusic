d97fe1dbde633f24afee6ea123f36e294e68c039
Dremle <1749946074@qq.com>
2018-01-05 16:01:11 +0800

    音乐推荐通过后台请求数据填充显示、个人歌单显示

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,12 @@
 //index.js
 //获取应用实例
-var sliderWidth = 98;
+var sliderWidth = 98
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    tabs: ["个性推荐", "歌单", "主播电台"],
+    tabs: ["个性推荐", "我的音乐", "主播电台"],
     imageurls:["images/music.png","images/search.png","images/user.png"],
     icons: ['iconfont icon-resou-copy','iconfont icon-jian'],
     icontexts: ['每日推荐','热搜排行'],
@@ -14,8 +14,13 @@ Page({
     sliderOffset: 0,
     sliderLeft: 0,
     sliderwidth:0,
+    user_id:0,
+    win_width:0,
     recommend_win_width:0,
     recommend:[],
+    recommend_mv_list:[],
+    recommend_list_key:[1,2,3,4,5,6],
+    user_album:[],
   },
 
   onclick_1:function(){
@@ -44,6 +49,7 @@ Page({
   onLoad: function (options) {
     console.log("index加载");
     var that = this;
+    var user_data = (wx.getStorageSync('user_data'));
     wx.getSystemInfo({
       success: function (res) {
         sliderWidth = res.windowWidth / 3;
@@ -51,25 +57,56 @@ Page({
           sliderOffset: (res.windowWidth / that.data.tabs.length) * that.data.activeIndex,
           sliderwidth: sliderWidth,
           recommend_win_width:(res.windowWidth/3)-2,
+          win_width:res.windowWidth,
         });
       }
     });
+    console.log(user_data);
+
 
     wx.request({
-      url: "http://123.207.142.115:3000/recommend/resource",
+      url: "http://123.207.142.115:3000/top/playlist/highquality?limit=6",
       header: {
         'content-type': 'application/json' // 默认值
       },
       success:function(res){
-
         that.setData({
-          recommend: res.data.recommend,
-
-
-        })
+          recommend: res.data.playlists,
+        }),
         console.log(res.data)
       },
       
+    })    //精品歌单请求
+
+    wx.request({
+      url: "http://123.207.142.115:3000/personalized/mv",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success:function(res1){
+        that.setData({
+          recommend_mv_list:res1.data.result,
+        })
+        
+      },
+      
+    })   //滑动海报请求
+
+    wx.request({
+      url: "http://123.207.142.115:3000/user/playlist",
+      data:{
+        uid:user_data.id,
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        that.setData({
+          user_album:res.data.playlist,
+        }),
+        console.log(res.data)
+      },
+
     })
 
   },
