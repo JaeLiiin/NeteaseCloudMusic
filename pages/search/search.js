5f7fb0d24ab59851b9836adb76e0b65ece4106e5
Dremle <1749946074@qq.com>
2018-01-10 14:59:34 +0800

    搜索界面功能更新

--- pages/search/search.js
+++ pages/search/search.js
@@ -5,31 +5,66 @@ Page({
    * 页面的初始数据
    */
   data: {
-
     inputShowed: false,
-    inputVal: ""
-  
+    inputVal: "",
+    search_showed:false,
+    search_result:[],
+    result_showed:false,
+    a:6,
   },
+
+
+
+  search:function(e){
+    var that = this;
+    console.log(this.data.inputVal);
+    this.setData({
+      search_showed:false,
+    })
+    wx.request({
+      url: 'http://123.207.142.115:3000/search?keywords='+that.data.inputVal+'&limit=30',
+      header:{
+        'content-type' : 'application:json'    //默认值
+      },
+      success:function(res){
+        that.setData({
+          search_result:res.data.result,
+          result_showed:true,
+        }),
+        console.log(that.data.search_result)
+      },
+    })
+  },      //搜索功能
+
+
 
   showInput: function () {
     this.setData({
       inputShowed: true
     });
   },
+
+
   hideInput: function () {
     this.setData({
       inputVal: "",
-      inputShowed: false
+      inputShowed: false,
+      search_showed:false,
     });
   },
+
+
   clearInput: function () {
     this.setData({
       inputVal: ""
     });
   },
+
+
   inputTyping: function (e) {
     this.setData({
-      inputVal: e.detail.value
+      inputVal: e.detail.value,
+      search_showed:true,
     });
   },
 
@@ -37,7 +72,19 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var that = this;
+    wx.request({
+      url: 'http://123.207.142.115:3000/search?keywords=sway&limit=1',
+      header: {
+        'content-type': 'application:json'    //默认值
+      },
+      success: function (res) {
+        that.setData({
+          search_result: res.data.result,
+        }),
+          console.log(res.data)
+      },
+    })
   },
 
   /**
@@ -51,7 +98,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
